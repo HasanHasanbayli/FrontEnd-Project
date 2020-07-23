4a361c95252d5d1682202776270b6b285fdd944c
Hasan Hasnbayli <64782625+HasanHasanbayli@users.noreply.github.com>
2020-07-24 00:28:53 +0400

    Add files via upload

---
+++ p116-15-07-2020-frontend-layihe-HasanHasanbeyli/Javascript/accordion.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(document).on("click", ".card-header", function () {
+        if ($(this).next()[0] != $(".active")[0]) {
+            $(".active").slideUp("slow", function () {
+                $(this).removeClass("active")
+            });
+            $(this).next().slideDown("slow", function () {
+                $(this).addClass("active")
+            });
+        }else{
+            $(".active").slideUp("slow", function () {
+                $(this).removeClass("active")
+            });
+        }
+    })
+    $(document).on("click", ".card-header", function(){
+        $(this).cild.removeClass("d-none")
+    })
+
+})
